3ae05812156cf3f0b83d6eac26d196fe27196a2b
Matthew Dias <matt.dias6@gmail.com>
2019-01-26 14:42:18 -0500

    created Pourer endpoint and object. Created Drinker object

---
+++ Server/Sources/FebrewaryServer/Drinker.swift
@@ -0,0 +1,45 @@
+//
+//  Drinker.swift
+//  COpenSSL
+//
+//  Created by Matthew Dias on 1/26/19.
+//
+
+import Foundation
+import StORM
+import PostgresStORM
+
+class Drinker: PostgresStORM {
+    var id: Int = 0
+    var drinkerToken: String = ""
+
+    override open func table() -> String { return "drinker" }
+
+    override func to(_ this: StORMRow) {
+        id = this.data["id"] as? Int ?? 0
+        drinkerToken = this.data["drinkertoken"] as? String ?? "\(Foundation.UUID())"
+    }
+
+    func rows() -> [Drinker] {
+        let rows = self.results.rows
+
+        guard !rows.isEmpty else { return []}
+
+        var drinkers = [Drinker]()
+
+        for row in rows {
+            let drinker = Drinker()
+            drinker.to(row)
+            drinkers.append(drinker)
+        }
+
+        return drinkers
+    }
+
+    func asDictionary() -> [String: Any] {
+        return [
+            "id": self.id,
+            "drinkerToken": "\(self.drinkerToken)",
+        ]
+    }
+}
--- Server/Sources/FebrewaryServer/main.swift
+++ Server/Sources/FebrewaryServer/main.swift
@@ -1,1 +1,68 @@
-print("Hello, world!")
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+
+import StORM
+import PostgresStORM
+
+PostgresConnector.host = "localhost"
+PostgresConnector.username = "perfect"
+PostgresConnector.password = "perfect"
+PostgresConnector.database = "febrewary"
+PostgresConnector.port = 5432
+
+let server = HTTPServer()
+server.serverPort = 8080
+
+let pourer = Pourer()
+try? pourer.setup()
+
+let drinker = Drinker()
+try? drinker.setup()
+
+var routes = Routes()
+
+func getPourerToken(request: HTTPRequest, response: HTTPResponse) {
+    do {
+        let objectQuery = Pourer()
+        try objectQuery.findAll()
+        var responseJson: [String: Any] = [:]
+
+        for row in objectQuery.rows() {
+            responseJson = row.asDictionary()
+        }
+
+        if !responseJson.isEmpty {
+            try response.setBody(json: responseJson)
+                        .completed(status: .ok)
+        } else {
+            let pourer = Pourer()
+
+            try pourer.save { id in
+                pourer.id = id as! Int
+            }
+
+            try response.setBody(json: pourer.asDictionary())
+                        .completed(status: .ok)
+        }
+
+    } catch let error as StORMError {
+        response.setBody(string: error.string())
+            .completed(status: .internalServerError)
+    } catch let error {
+        response.setBody(string: "\(error)")
+            .completed(status: .internalServerError)
+    }
+}
+
+routes.add(Route(method: .get, uri: "pourerToken", handler: getPourerToken))
+
+
+server.addRoutes(routes)
+
+do {
+    try server.start()
+} catch PerfectError.networkError(let err, let msg) {
+    print("Network error thrown: \(err) \(msg)")
+}
+
