4ec8507b68747f5e34204a1581fbffebd1ae483a
Matthew Dias <matt.dias6@gmail.com>
2019-02-16 19:46:32 -0500

    basic pouring UI

---
+++ App/Febrewary/Febrewary/Views/PourView/PourViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PourViewController.swift
+//  Febrewary
+//
+//  Created by Matthew Dias on 2/16/19.
+//  Copyright © 2019 Matt Dias. All rights reserved.
+//
+
+import UIKit
+
+class PourViewController: UIViewController {
+    @IBOutlet var inputTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func submitTapped(_ sender: Any) {
+    }
+}
