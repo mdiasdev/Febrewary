3e1d2830c6cfad6974fc95b3f8455b42bbf5f10c
Matt Dias <matt.dias6@gmail.com>
2019-06-16 13:43:03 -0400

    added screen for when no events are returned from the server

--- App/Febrewary/Febrewary/Views/Events/EventsViewController.swift
+++ App/Febrewary/Febrewary/Views/Events/EventsViewController.swift
@@ -10,18 +10,45 @@ import UIKit
 
 class EventsViewController: UIViewController {
     var transition = TopDownAnimator()
+    
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var containerView: UIView!
 
+    let noEventsView = Bundle.main.loadNibNamed("NoEventsView", owner: self, options: nil)?.first as! NoEventsView // FIXME: no force unwrapping
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        noEventsView.translatesAutoresizingMaskIntoConstraints = false
+        
+        containerView.addSubview(noEventsView)
+        NSLayoutConstraint.activate([
+            noEventsView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            noEventsView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            noEventsView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            noEventsView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+        ])
+        
+        noEventsView.titleLabel.text = "No Upcomming Events"
+    }
+    
     @IBAction func showAccount(_ sender: Any) {
         guard let accountVC = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "Account") as? UINavigationController else { return }
         
         accountVC.transitioningDelegate = self
         present(accountVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func segmentChanged(_ sender: Any) {
+        if segmentControl.selectedSegmentIndex == 0 {
+            noEventsView.titleLabel.text = "No Upcomming Events"
+        } else {
+            noEventsView.titleLabel.text = "No Past Events"
+        }
     }
 }
 
