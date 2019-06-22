3dbb476abac7e1c1aaa692a4892f440b682b933f
Matt Dias <matt.dias6@gmail.com>
2019-06-22 14:17:56 -0400

    cleaned up Assets a bit, added shell of Create Event screen, and hooked up close button

---
+++ App/Febrewary/Febrewary/Views/Events/Create Event/CreateEventViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CreateEventViewController.swift
+//  Febrewary
+//
+//  Created by Matthew Dias on 6/22/19.
+//  Copyright © 2019 Matt Dias. All rights reserved.
+//
+
+import UIKit
+
+class CreateEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+    @IBAction func didTapClose(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
