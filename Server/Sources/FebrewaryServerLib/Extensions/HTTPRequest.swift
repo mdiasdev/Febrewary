dd115473de2da24596d1876e97b59988bb94d443
Matt Dias <matt.dias6@gmail.com>
2019-07-23 21:09:20 -0400

    added fuzzy search for beers & brewers

--- Server/Sources/FebrewaryServerLib/Extensions/HTTPRequest.swift
+++ Server/Sources/FebrewaryServerLib/Extensions/HTTPRequest.swift
@@ -7,7 +7,7 @@ extension HTTPRequest {
     func queryParamsAsDictionary() -> [String: String] {
         var params: [String: String] = [:]
         for param in queryParams {
-            params[param.0] = param.1
+            params[param.0] = param.1.replacingOccurrences(of: "\"", with: "")
         }
 
         return params
