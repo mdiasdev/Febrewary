5aad490ea7f9a3017c402934d79b799945f1bfa1
Matt Dias <matt.dias6@gmail.com>
2019-09-03 20:05:50 -0400

    added NotificationManager to be able to post when a user logs in

---
+++ App/Febrewary/Febrewary/Managers/NotificationManager.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationManager.swift
+//  Febrewary
+//
+//  Created by Matthew Dias on 9/3/19.
+//  Copyright © 2019 Matt Dias. All rights reserved.
+//
+
+import Foundation
+
+enum NotificationName: String {
+    case loggedIn
+}
+
+struct NotificationManager {
+    
+    static func notifyAll(of name: Notification.Name) {
+        NotificationCenter.default.post(name: name, object: nil)
+    }
+}
+
+extension NSNotification.Name {
+    static let loggedIn = Notification.Name(NotificationName.loggedIn.rawValue)
+}
