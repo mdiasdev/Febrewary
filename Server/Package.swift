6d9a07dffbe01979b4e04c4bd97fb31dce0ce146
Matt Dias <matt.dias6@gmail.com>
2019-11-17 11:02:32 -0500

    updated package file

--- Server/Package.swift
+++ Server/Package.swift
@@ -1,28 +1,20 @@
 // swift-tools-version:4.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// Generated automatically by Perfect Assistant
+// Date: 2019-10-13 19:10:15 +0000
 import PackageDescription
 
 let package = Package(
-    name: "FebrewaryServer",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
-        .package(url: "https://github.com/SwiftORM/Postgres-StORM.git", from: "3.1.0"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "3.2.0"),
-        .package(url: "https://github.com/IBM-Swift/Configuration.git", from:"3.0.3")
-        // TODO: Swagger docs https://github.com/mczachurski/Swiftgger
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "FebrewaryServerApp",
-            dependencies: ["FebrewaryServerLib", "PerfectHTTPServer", "PostgresStORM"]),
-        .target(name: "FebrewaryServerLib",
-                dependencies: ["PerfectHTTPServer", "PostgresStORM", "PerfectCrypto", "Configuration"]),
-        .testTarget(
-            name: "FebrewaryServerLibTests",
-            dependencies: ["FebrewaryServerLib", "PerfectHTTPServer", "PostgresStORM", "PerfectCrypto", "Configuration"]),
-        ]
+	name: "FebrewaryServer",
+	dependencies: [
+		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.0.0"..<"4.0.0"),
+		.package(url: "https://github.com/SwiftORM/Postgres-StORM.git", "3.1.0"..<"4.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", "3.2.0"..<"4.0.0"),
+		.package(url: "https://github.com/IBM-Swift/Configuration.git", "3.0.3"..<"4.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", "3.0.0"..<"4.0.0")
+	],
+	targets: [
+		.target(name: "FebrewaryServerApp", dependencies: ["FebrewaryServerLib", "PerfectHTTPServer", "PostgresStORM"]),
+		.target(name: "FebrewaryServerLib", dependencies: ["PerfectHTTPServer", "PostgresStORM", "PerfectCrypto", "Configuration"]),
+		.testTarget(name: "FebrewaryServerLibTests", dependencies: ["FebrewaryServerLib", "PerfectHTTPServer", "PostgresStORM", "PerfectCrypto", "Configuration"])
+	]
 )
